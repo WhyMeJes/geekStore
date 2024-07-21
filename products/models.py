cfa0529dd7e4c2620931239371a8bfc95d868488
WhyMeJesus <markantonyan@mail.ru>
2024-07-21 22:18:12 +0700

    изменил models.py

--- products/models.py
+++ products/models.py
@@ -6,6 +6,9 @@ class ProductCategory(models.Model):
     name = models.CharField(max_length=64,unique=True) #CharField, TextField это тип данных, в скобках - доп условия типо unique, blank (если true то поле может быть пустым)
     description = models.TextField(blank=True)
 
+    def __str__(self):
+        return self.name
+
 class Products(models.Model):
     name = models.CharField(max_length=256)
     image = models.ImageField(upload_to='products_images',blank=True) #upload_to - папка куда будут выгружаться фотографии
@@ -14,3 +17,6 @@ class Products(models.Model):
     price = models.DecimalField(max_digits=8,decimal_places=2,default=0) #Тип данных для денег, 2 знака после запятой, максимум цифр в целой части 8, по умолчанию поле равно 0
     quantity = models.PositiveIntegerField(default=0) #Положительное целое число
     category = models.ForeignKey(ProductCategory, on_delete=models.CASCADE) #Внешний ключ ссылающийся на таблицу ProductCategory, при удалении Категории товары также удалятся
+
+    def __str__(self):
+        return self.name
