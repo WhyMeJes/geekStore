2a990a4a35db4db98f52a50137c5b6f5c9e7eb20
WhyMeJesus <markantonyan@mail.ru>
2024-07-21 19:12:04 +0700

    Создал модели и добавил данные в БД

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from products.models import ProductCategory, Products
 
 # Register your models here.
+admin.site.register(ProductCategory)
+admin.site.register(Products)
